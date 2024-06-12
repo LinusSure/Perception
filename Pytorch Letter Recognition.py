0c438c85d6c1eb57c9423cbc163f38d166c27b85
Linus Schuermann <schuermann@campus.tu-berlin.de>
2024-06-12 10:32:21 +0200

    imports

--- Pytorch Letter Recognition.py
+++ Pytorch Letter Recognition.py
@@ -1,2 +1,7 @@
-import torch
 import numpy as np
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+from time import time
+from torchvision import datasets, transforms
+from torch import nn, optim
